292116ba7d0a0b9885ae91cc28e8663deebd2908
Jon Petersen <jon.petersen@outlook.dk>
2025-02-21 17:32:53 +0100

    Task 2 Start on project

---
+++ Task1_2.py
@@ -0,0 +1,19 @@
+####################################### TASK 2 #######################################################
+
+#Representing Dataframe.
+
+import pandas as pd
+
+df = pd.read_excel("995,000_rows.xlsx")
+
+df.head()
+
+'''
+Text about why choosing pandas as the main Dataframe object.
+
+
+
+
+
+
+'''
